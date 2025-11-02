59f327b9a8a02b14ded44e8c69ec8200577127e1
denijstransport <denijstransport@hotmail.com>
2025-11-02 21:44:01 +0100

    Create Diensten component with service information

---
+++ app/diensten/page.tsx
@@ -0,0 +1,8 @@
+export default function Diensten(){
+return (
+<div className="prose max-w-3xl">
+<h1>Diensten</h1>
+<p>Distributie, koerierswerk, sneltransport, geconditioneerd vervoer. Neem contact op voor maatwerk.</p>
+</div>
+);
+}
